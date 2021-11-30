5bac3a15467a37e300ab9a7acd0f361b96b0ff80
Daniella Dutra <danivetdutra@gmail.com>
2021-11-30 15:39:12 -0300

    Criada chamada API

--- src/App.js
+++ src/App.js
@@ -1,1 +1,40 @@
+import React from 'react';
+import api from './api';
+import css from './index.css';
 
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filmes: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('Batman');
+
+    // console.log(response.data);
+    this.setState({
+      filmes: response.data,
+    });
+  }
+
+  render() {
+    const { filmes } = this.state;
+
+    return (
+      <>
+        <h1>Lista de filmes</h1>
+        {filmes.map((filmes) => (
+          <li key={filmes.show.id}>
+            <h2>Título: {filmes.show.name}</h2>
+            <p className="cursor">{filmes.show.url} </p>
+          </li>
+        ))}
+      </>
+    );
+  }
+}
+
+export default App;
